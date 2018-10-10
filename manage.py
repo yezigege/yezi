f1ff80601890446c56b7b36e5c3b14033e858683
yezigege <1757423278@qq.com>
2018-10-10 15:34:50 +0800

    集成数据库迁移的命令

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 from flask import Flask, session
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 from flask_session import Session
 from flask_wtf import CSRFProtect
@@ -44,6 +45,10 @@ CSRFProtect(app)
 Session(app)
 
 manager = Manager(app)
+# 将 app 与 db 关联
+Migrate(app, db)
+# 将迁移命令添加到manager中
+manager.add_command('db', MigrateCommand)
 
 @app.route('/')
 def index():
