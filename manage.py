714a39687c56f85ade44ce5b27e7fa23c1b56400
yezigege <1757423278@qq.com>
2018-10-10 15:24:45 +0800

    Session的保存位置指定&测试

--- manage.py
+++ manage.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, session
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask_session import Session
 from flask_wtf import CSRFProtect
 from redis import StrictRedis
 
 
 class  Config(object):
     """工程配置信息"""
+    SECRET_KEY = "b'xagZkmSwJyRiWW4EI7Tc4/aCu8X91oWtEMOcbub3ORXSz/252XMd2nGFDV9cl5hU'"
+
     DEBUG = True
     # 数据库的配置信息
     SQLALCHEMY_DATABASW_URI = "mysql://root:mysql@127.0.0.1:3306/chonggou"
@@ -14,6 +17,17 @@ class  Config(object):
     # redis 的配置
     REDIS_HOST = "127.0.0.1"
     REDIS_PORT = 6379
+
+    # Session保存配置
+    SESSION_TYPE = "redis"
+    # 开启session签名
+    SESSION_USE_SIGNER = True
+    # 指定 Session 保存的 redis
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    # 设置需要过期
+    SESSION_PERMANENT = False
+    # 设置过期时间
+    PERMANENT_SESSION_LIFETIME = 86400 * 2
 
 app = Flask(__name__)
 # 加载配置
@@ -26,8 +40,11 @@ redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 # 开启当前项目的 CSRF 保护，只做服务验证功能
 CSRFProtect(app)
 
+Session(app)
+
 @app.route('/')
 def index():
+    session["name"] = "yezi"
     return "yezi "
 
 
