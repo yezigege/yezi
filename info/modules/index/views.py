3d3f0c09be9ca53fdc73767397fa06a723f6a257
yezigege <1757423278@qq.com>
2018-10-13 20:50:32 +0800

    判断用户是否登录的右上角内容显示

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,12 +1,32 @@
-from flask import render_template, current_app
+from flask import render_template, current_app, session
 
 from info import redis_store
+from info.models import User
 from . import index_blu
 
 
 @index_blu.route('/')
 def index():
-    return render_template('news/index.html')
+    """
+    显示首页
+    1. 如果用户已经登录，将当前登录用户的数据传到模板中，供模版显示
+    :return:
+    """
+
+    user_id = session.get("user_id", None)
+    user = None
+    if user_id:
+        try:
+            user = User.query.get(user_id)
+        except Exception as e:
+            current_app.logger.error(e)
+
+    data = {
+        "user": user.to_dict() if user else None
+    }
+
+
+    return render_template('news/index.html', data=data)
 
 
 # 在打开网页的时候，浏览器默认回去请求/+favicon.ico做网站标签的小图标
