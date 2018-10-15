e84e2d2be96e24d2f4523f755b6ddfedeae1d6f9
yezigege <1757423278@qq.com>
2018-10-15 10:09:52 +0800

    首页的排行列表新闻展示

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, current_app, session
 
 from info import redis_store
-from info.models import User
+from info.models import User, News
 from . import index_blu
 
 
@@ -12,17 +12,33 @@ def index():
     1. 如果用户已经登录，将当前登录用户的数据传到模板中，供模版显示
     :return:
     """
-
+    # 显示用户是否登录的逻辑
     user_id = session.get("user_id", None)
     user = None
     if user_id:
+        # 尝试查询用户的模型
         try:
             user = User.query.get(user_id)
         except Exception as e:
             current_app.logger.error(e)
 
+    # 右侧的新闻排行的逻辑
+    news_list = []
+    try:
+        # 这里查询出来的news_list 是一个对象
+        news_list = News.query.order_by(News.clicks.desc()).limit(6)
+    except Exception as e:
+        current_app.logger.error(e)
+
+    # 定义一个空的字典列表，里面装的就是字典
+    news_dict_li = []
+    # 遍历对象列表，将对象的字典添加到字典列表中
+    for news in news_list:
+        news_dict_li.append(news.to_basic_dict())
+
     data = {
-        "user": user.to_dict() if user else None
+        "user": user.to_dict() if user else None,
+        "news_dict_li": news_dict_li
     }
 
 
