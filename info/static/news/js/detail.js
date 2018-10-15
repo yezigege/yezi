45f313a55de85807542c133a57f9c4de8c3c98fa
yezigege <1757423278@qq.com>
2018-10-15 21:07:19 +0800

    评论点赞和取消点赞的后端逻辑实现

--- info/static/news/js/detail.js
+++ info/static/news/js/detail.js
@@ -133,6 +133,7 @@ $(function(){
                     $('.comment_sub').blur();
                     // 清空输入框内容
                     $(".comment_input").val("")
+                    updateCommentCount()
                 }else {
                     alert(resp.errmsg)
                 }
@@ -232,6 +233,7 @@ $(function(){
                         $this.prev().val('')
                         // 关闭
                         $this.parent().hide()
+                        updateCommentCount()
                     }else {
                         alert(resp.errmsg)
                     }
@@ -250,3 +252,8 @@ $(function(){
 
     })
 })
+
+function updateCommentCount() {
+    var count = $('.comment_list').length
+    $('.comment_count').html(count+"条评论")
+}
